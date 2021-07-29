7eecd50aaa71369c43a8d63830e2a7c7989220da
zozulia-developer <kirillzozulya95@gmail.com>
2021-07-29 13:37:50 +0300

    rename project dir + added one session for all requests

--- fastapi_project/apps/posts/dependencies.py
+++ fastapi_project/apps/posts/dependencies.py
@@ -1,15 +1,24 @@
+import aiohttp
 from aiohttp import ClientSession
 
 from typing import List, Dict, Any
 
+from fastapi import Depends
+
 from .models import Post, CreatePostParams, CreatedPost, PostDetails, UpdatePostParams
 
 
+async def get_session() -> ClientSession:
+    async with ClientSession(trust_env=True) as session:
+        yield session
+
+
 class PostRepository:
-    def __init__(self) -> None:
+    def __init__(self, session) -> None:
         self._endpoint_posts = "https://jsonplaceholder.typicode.com/posts"
         self._endpoint_users = "https://jsonplaceholder.typicode.com/users"
         self._endpoint_comments = "https://jsonplaceholder.typicode.com/comments"
+        self._session = session
 
     async def list_posts(self) -> List[Post]:
         raw_posts = await self._list_posts()
@@ -36,53 +45,47 @@ class PostRepository:
         return self._convert_post_details(raw_post)
 
     async def _list_posts(self) -> List[Dict[str, Any]]:
-        async with ClientSession(trust_env=True) as session:
-            resp_posts = await session.get(self._endpoint_posts)
-            raw_posts = await resp_posts.json()
-            return raw_posts
+        resp_posts = await self._session.get(self._endpoint_posts)
+        raw_posts = await resp_posts.json()
+        return raw_posts
 
     async def _post_details(self, post_id: int) -> Dict[str, Any]:
-        async with ClientSession(trust_env=True) as session:
-            resp = await session.get(f'{self._endpoint_posts}/{post_id}')
-            raw_post = await resp.json()
-            resp_comments = await session.get(f"{self._endpoint_comments}?postId={post_id}")
-            raw_comments = await resp_comments.json()
-            resp_user = await session.get(f"{self._endpoint_users}/{raw_post['userId']}")
-            raw_user = await resp_user.json()
-            raw_post['author'] = {
-                'id': raw_user['id'],
-                'name': raw_user['name'],
-                'email': raw_user['email']
-            }
-            raw_post['comments'] = raw_comments
-            return raw_post
+        resp = await self._session.get(f'{self._endpoint_posts}/{post_id}')
+        raw_post = await resp.json()
+        resp_comments = await self._session.get(f"{self._endpoint_comments}?postId={post_id}")
+        raw_comments = await resp_comments.json()
+        resp_user = await self._session.get(f"{self._endpoint_users}/{raw_post['userId']}")
+        raw_user = await resp_user.json()
+        raw_post['author'] = {
+            'id': raw_user['id'],
+            'name': raw_user['name'],
+            'email': raw_user['email']
+        }
+        raw_post['comments'] = raw_comments
+        return raw_post
 
     async def _list_users(self) -> List[Dict[str, Any]]:
-        async with ClientSession(trust_env=True) as session:
-            resp = await session.get(self._endpoint_users)
-            raw_users = await resp.json()
-            return raw_users
+        resp = await self._session.get(self._endpoint_users)
+        raw_users = await resp.json()
+        return raw_users
 
     async def _list_comments(self) -> List[Dict[str, Any]]:
-        async with ClientSession() as session:
-            resp = await session.get(self._endpoint_comments)
-            raw_comments = await resp.json()
-            return raw_comments
+        resp = await self._session.get(self._endpoint_comments)
+        raw_comments = await resp.json()
+        return raw_comments
 
     async def _create_post(self, user: CreatePostParams) -> Dict[str, Any]:
-        async with ClientSession(trust_env=True) as session:
-            resp = await session.post(self._endpoint_posts, json=user.dict())
-            raw_post = await resp.json()
-            return raw_post
+        resp = await self._session.post(self._endpoint_posts, json=user.dict())
+        raw_post = await resp.json()
+        return raw_post
 
     async def _update_post(self, post: UpdatePostParams, post_id: int) -> Dict[str, Any]:
-        async with ClientSession(trust_env=True) as session:
-            resp = await session.put(
-                f'{self._endpoint_posts}/{post_id}',
-                json=post.dict()
-            )
-            raw_post = await resp.json()
-            return raw_post
+        resp = await self._session.put(
+            f'{self._endpoint_posts}/{post_id}',
+            json=post.dict()
+        )
+        raw_post = await resp.json()
+        return raw_post
 
     def _convert_post(self, raw_post: Dict[str, Any]) -> Post:
         return Post(**raw_post)
@@ -98,10 +101,9 @@ class PostRepositoryFactory:
     def __init__(self):
         self._repo = None
 
-    def __call__(self) -> PostRepository:
-        if self._repo is None:
-            self._repo = PostRepository()
-        return self._repo
+    def __call__(self, session: ClientSession = Depends(get_session)) -> PostRepository:
+        _repo = PostRepository(session)
+        return _repo
 
 
 get_post_repository = PostRepositoryFactory()
--- fastapi_project/apps/users/dependencies.py
+++ fastapi_project/apps/users/dependencies.py
@@ -1,14 +1,21 @@
 from typing import List, Dict, Any
 
 import aiohttp
+from fastapi import Depends
 
 from .models import CreateUserParams, User
 
 
+async def get_session() -> aiohttp.ClientSession:
+    async with aiohttp.ClientSession(trust_env=True) as session:
+        yield session
+
+
 class UserRepository:
-    def __init__(self):
+    def __init__(self, session) -> None:
         self._users: List[User] = []
         self._serial = len(self._users)
+        self._session = session
 
     async def create_user(self, user: CreateUserParams) -> User:
         user = User(id=self._serial, **user.dict())
@@ -21,8 +28,9 @@ class UserRepository:
 
 
 class JSONPlaceholderUserRepository(UserRepository):
-    def __init__(self):
+    def __init__(self, session):
         self._endpoint = "https://jsonplaceholder.typicode.com/users"
+        self._session = session
 
     async def create_user(self, user: CreateUserParams) -> User:
         raw_user = await self._create_user(user)
@@ -33,16 +41,14 @@ class JSONPlaceholderUserRepository(UserRepository):
         return [self._convert_user(raw_user) for raw_user in raw_users]
 
     async def _create_user(self, user: CreateUserParams) -> Dict[str, Any]:
-        async with aiohttp.ClientSession(trust_env=True) as session:
-            resp = await session.post(self._endpoint, json=user.dict())
-            raw_user = await resp.json()
-            return raw_user
+        resp = await self._session.post(self._endpoint, json=user.dict())
+        raw_user = await resp.json()
+        return raw_user
 
     async def _list_users(self) -> List[Dict[str, Any]]:
-        async with aiohttp.ClientSession(trust_env=True) as session:
-            resp = await session.get(self._endpoint)
-            raw_users = await resp.json()
-            return raw_users
+        resp = await self._session.get(self._endpoint)
+        raw_users = await resp.json()
+        return raw_users
 
     def _convert_user(self, raw_user: Dict[str, Any]) -> User:
         return User(**raw_user)
@@ -52,10 +58,9 @@ class UserRepositoryFactory:
     def __init__(self):
         self._repo = None
 
-    def __call__(self) -> UserRepository:
-        if self._repo is None:
-            self._repo = JSONPlaceholderUserRepository()
-        return self._repo
+    def __call__(self, session: aiohttp.ClientSession = Depends(get_session)) -> UserRepository:
+        _repo = JSONPlaceholderUserRepository(session)
+        return _repo
 
 
 get_user_repository = UserRepositoryFactory()
